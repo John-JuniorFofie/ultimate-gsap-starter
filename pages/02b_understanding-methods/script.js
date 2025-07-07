aea4a284464935b1b1ec120d401d88876db8dac6
John Junior Fofie <johnfofie31@gmail.com>
2025-07-07 09:00:02 +0000

    Update script.js

--- pages/02b_understanding-methods/script.js
+++ pages/02b_understanding-methods/script.js
@@ -1,46 +1,83 @@
-import gsap from 'gsap';
-const play = document.querySelector(".Play");
-const pause = document.querySelector(".Pause");
-const Resume = document.querySelector(".Resume");
-const Restart = document.querySelector(".Restart");
-const Kill = document.querySelector(".Kill");
+import gsap from "gsap";
+
+
+
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+const resume = document.querySelector(".resume");
+const restart = document.querySelector(".restart");
+const reverse = document.querySelector(".reverse");
+const repeat = document.querySelector(".repeat");
+const kill = document.querySelector(".kill");
 const yoyo = document.querySelector(".yoyo");
+const seek = document.querySelector(".seek");
+const progress = document.querySelector(".progress");
+const timescale = document.querySelector(".timescale");
+const halfRestart = document.querySelector(".half-restart");
+const complete = document.querySelector(".complete");
 
-const animation =gsap.to('.box',{
-opacity:1,
-scale:1.25,
-duration:4,
-repeat:-1,
-yoyo:true,
-rotate:360,
-borderRadius:'50%',
-// ease: "power2.in"      //starts slow, ends fast
-// ease: "power2.out"     //starts fast, ends slow
-// ease: "power2.inOut"   //slow-fast-slow
 
-})
-
-play.addEventListener("click", ()=>{
+const animation = gsap.to('.box', {
+  opacity: 1,
+  scale: 1.25,
+  duration: 3,
+  repeat: -1,
+  yoyo: true,
+  rotate: 360,
+  borderRadius: '50%',
+  // ease: "power2.in",
+  // ease: "power2.out",
+  // ease: "power2.inOut"
+});
+play.addEventListener("click", () =>{
     animation.play();
 });
-pause.addEventListener("click", ()=>{
+
+pause.addEventListener("click", () =>{
     animation.pause();
 });
-resume.addEventListener("click", ()=>{
+
+resume.addEventListener("click", () =>{
     animation.resume();
 });
-restart.addEventListener("click", ()=>{
+
+restart.addEventListener("click", () =>{
     animation.restart();
 });
-reverse.addEventListener("click", ()=>{
+
+reverse.addEventListener("click", () =>{
     animation.reverse();
 });
-Kill.addEventListener("click", ()=>{
-    animation.Kill();
+
+kill.addEventListener("click", () =>{
+    animation.kill();
 });
-yoyo.addEventListener("click", ()=>{
+
+yoyo.addEventListener("click", () =>{
     animation.yoyo(true);
 });
-repeat.addEventListener("click", ()=>{
-    animation.yoyo(2);
+
+repeat.addEventListener("click", () =>{
+    animation.repeat(2);
+});
+
+seek.addEventListener("click", () => {
+  animation.seek(1); // Jump to 1 second
+});
+
+progress.addEventListener("click", () => {
+  animation.progress(0.5); // 50%
+});
+
+timescale.addEventListener("click", () => {
+  animation.timeScale(2); // Speed x2
+});
+
+halfRestart.addEventListener("click", () => {
+  animation.pause(0.5); // Pause first
+  animation.progress(0.5).restart(); // Start from 50%
+});
+
+complete.addEventListener("click", () => {
+  animation.progress(1); // Instantly finish
 });
